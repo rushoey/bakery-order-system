ef6e87e33afa5d1d2094c28118f5853f51c5f735
joey <joseagzzes@gmail.com>
2026-08-28 17:53:00 -0600

    agregue funcion de crear orden

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+import sys
 orders = []
+def crear_orden() :
+    name.lower = input("\nIngresa el nombre del cliente (o escribe 'salir' para abandonar): ").strip()
+    if name.lower() == 'salir' :
+       sys.exit 
+    phone = input("Ingresa el numero de telefono del cliente: ").strip()
+    date = input("Ingresa la fecha para recoger (dd-mm): ").strip()
+    amount = int(input("Ingresa la cantidad de hogazas: "))
+
+    new_order = {
+        "name": name,
+        "phone": phone,
+        "date": date,
+        "amount": amount
+    }
+    orders.append(crear_orden)
+    print(f"Orden creada con exito para {name}!")
+
 
 print("\n==========================================================")
 print("----- Bienvenid@ al sistema de ordenes de madriguera -----")
@@ -10,5 +28,7 @@ print("3. Cancelar ordenes")
 print("4. Ver hogazas disponibles")
 print("5. Salir del sistema")
 
-option = input("\nElije la funcion que quieras utilizar ej. '1': ")
+option = int(input("\nElije la funcion que quieras utilizar ej. '1': "))
 
+if (option == 1) :
+    crear_orden()
