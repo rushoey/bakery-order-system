3b42d2c66b2620d084f0350f6c953f92dec1f56c
joey <joseagzzes@gmail.com>
2026-08-29 21:02:30 -0600

    agregue id para las ordenes, funcion para regresar al menu[lowk la voy a cambiar despues], funcion para cancelar ordenes, y para ver un registro de las ordenes canceladas

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 orders = []
+ordenes_eliminadas = []
+id = 0
 def menu_principal() :
     print("\n==============================================================")
     print("-- Bienvenid@ al sistema de ordenes de madriguera de hogazas--")
@@ -7,8 +9,9 @@ def menu_principal() :
     print("\n1. Crear orden")
     print("2. Ver ordenes")
     print("3. Cancelar ordenes")
-    print("4. Ver hogazas disponibles")
-    print("5. Salir del sistema")
+    print("4. Registro ordenes eliminadas")
+    print("5. Ver hogazas disponibles")
+    print("6. Salir del sistema")
 
     option = int(input("\nElije la funcion que quieras utilizar ej. '1': "))
 
@@ -16,16 +19,26 @@ def menu_principal() :
         crear_orden()
     elif (option == 2) :
         ver_ordenes()
+    elif (option == 3) :
+        cancelar_orden()
+    elif (option == 4) :
+        registro_eliminadas()
+
+def regresar_menu() :
+    regresar = input("\n\n-----Deseas regresar al menu? teclea 'menu'-----: ")
+    if regresar.lower() == 'menu' :
+       menu_principal()
 
 def crear_orden() :
-    name = input("\nIngresa el nombre del cliente (o escribe 'menu' para regresar al menu): ").strip()
-    if name.lower() == 'menu' :
-       menu_principal()
+    name = input("\nIngresa el nombre del cliente: ").strip()
     phone = input("Ingresa el numero de telefono del cliente: ").strip()
     date = input("Ingresa la fecha para recoger (dd-mm): ").strip()
     amount = int(input("Ingresa la cantidad de hogazas: "))
+    global id
+    id += 1
 
     new_order = {
+        "id": id,
         "name": name,
         "phone": phone,
         "date": date,
@@ -33,15 +46,37 @@ def crear_orden() :
     }
     orders.append(new_order)
     print(f"\nOrden creada con exito para {name}!")
-    menu_principal()
+    regresar_menu()
 
 def ver_ordenes() :
     print("\n---Estas son las ordenes pendientes---")
-    for index, order in enumerate(orders, 1):
-        print(f"Order #{index}:")
+    for index, order in enumerate(orders) :
+        print(f"Orden #{order['id']}:")
         print(f"    Nombre: {order['name']}")
         print(f"    Numero de telefono: {order['phone']}")
         print(f"    Fecha: {order['date']}")
         print(f"    Cantidad de hogazas: {order['amount']}\n")
+    regresar_menu()
+
+def cancelar_orden() :
+    id_eliminar = int(input("\n---Ingresa el numero de orden que quieras eliminar---"))
+    for index, order in enumerate(orders) :
+        if (id_eliminar == order['id']) :
+            ordenes_eliminadas.append(orders.pop(index))
+            print(f"\nOrden #{id_eliminar} eliminada con exito")
         
+    regresar_menu()
+
+def registro_eliminadas() :
+    print("\n---Estas son las ultimas ordenes que fueron eliminadas ---")
+    for index, orden_eliminada in enumerate(ordenes_eliminadas, 1):
+        print(f"Orden #{orden_eliminada['id']}:")
+        print(f"    Nombre: {orden_eliminada['name']}")
+        print(f"    Numero de telefono: {orden_eliminada['phone']}")
+        print(f"    Fecha: {orden_eliminada['date']}")
+        print(f"    Cantidad de hogazas: {orden_eliminada['amount']}\n")
+
+    regresar_menu()
+
+
 menu_principal()
